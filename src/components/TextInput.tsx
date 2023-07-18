61a46ec2155a130c2b6aa4ac53e88153390d8380
Samuel Nicholls <samuel.nicholls@hey.com>
2023-07-18 09:55:35 +0100

    Styling improvements

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -8,13 +8,14 @@ export type TextInputProps = {
   placeholder?: string;
   type: "text" | "email" | "password";
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
-  error?: string;
+  errors?: any;
 };
 
-const TextInput: FC<TextInputProps> = ({ id, value, label, name, placeholder, type, onChange, error }) => {
+const TextInput: FC<TextInputProps> = ({ id, value, label, name, placeholder, type, onChange }) => {
+  const inputError = false
   return (
     <div>
-      {label && <label htmlFor={id} className={`block mb-2 text-sm font-medium text-gray-900 ${error && "text-red-600"}`}>{label}</label>}
+      {label && <label htmlFor={id} className={`block mb-2 text-sm font-medium text-gray-900 ${inputError && "text-red-600"}`}>{label}</label>}
       <input
         id={id}
         type={type}
@@ -22,9 +23,9 @@ const TextInput: FC<TextInputProps> = ({ id, value, label, name, placeholder, ty
         name={name}
         placeholder={placeholder}
         onChange={onChange}
-        className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${error && "border-red-500 placeholder-red-700"}`}
+        className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${inputError && "border-red-500 placeholder-red-700"}`}
       />
-      {error && <p className="mt-2 text-sm text-red-600">{error}</p>}
+      {inputError && <p className="mt-2 text-sm text-red-600">{inputError}</p>}
     </div>
   )
 
--- src/schemas.ts
+++ src/schemas.ts
@@ -1,8 +1,8 @@
 import { z } from "Zod";
 
 export const signUpSchema = z.object({
-  firstName: z.string(),
-  lastName: z.string(),
+  firstName: z.string().min(1, { message: "First name is required" }),
+  lastName: z.string().min(1, { message: "Last name is required" }),
   emailAddress: z.string().email(),
   password: z.string(),
   terms: z.boolean()
